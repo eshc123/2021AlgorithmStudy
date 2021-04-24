7880770e284cfba9199e140b805a409d0a25f381
eshc@naver.com <eshc@naver.com>
2021-04-25 00:52:53 +0900

    init & 14681

---
+++ src/main/Baekjoon/14681.kt
@@ -0,0 +1,26 @@
+import java.util.*
+import java.io.*
+
+//조건문 기본 문제
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var st1 = StringTokenizer(br.readLine())
+    var st2 = StringTokenizer(br.readLine())
+    val a = Integer.parseInt(st1.nextToken())
+    val b = Integer.parseInt(st2.nextToken())
+    if(a>0 && b>0){
+        println('1')
+        return
+    }
+    else if(a>0 && b<0) {
+        println('4')
+        return
+    }
+    else if(a<0 && b>0) {
+        println('2')
+        return
+    }
+    else(
+            println('3')
+            )
+}
