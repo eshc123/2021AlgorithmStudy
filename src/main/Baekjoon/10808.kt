9452c4f961003c130603189ed514d2b9f8150e1f
eshc@naver.com <eshc@naver.com>
2021-04-25 14:03:20 +0900

    10808

--- src/main/Baekjoon/10808.kt
+++ src/main/Baekjoon/10808.kt
@@ -5,14 +5,10 @@ import java.io.*
 fun main(){
     val br = BufferedReader(InputStreamReader(System.`in`))
     val st1 = StringTokenizer(br.readLine())
-    val map : MutableMap<Char,Int> = mutableMapOf()
     val array = Array(26) {0}
-    for(i in 'a'..'z'){
-        map[i] = i-'a'
-    }
 
     st1.nextToken().forEach {
-        array[map[it]!!] += 1
+        array[it-'a'] += 1
     }
     array.forEach {
         print("$it ")
