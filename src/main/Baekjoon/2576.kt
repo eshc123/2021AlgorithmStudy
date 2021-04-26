eb6220b45be32b7028dc60c2415066ab55f034b3
eshc@naver.com <eshc@naver.com>
2021-04-27 07:15:34 +0900

    2576

---
+++ src/main/Baekjoon/2576.kt
@@ -0,0 +1,22 @@
+import java.util.*
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    var min = 99
+    var sum = 0
+    for(i in 0..6) {
+        val a = StringTokenizer(br.readLine()).nextToken().toInt()
+        if (a % 2 == 1 ) {
+            sum += a
+            if (min > a ) {
+                min = a
+            }
+        }
+    }
+    if ( sum == 0 ) bw.write("-1")
+    else bw.write("$sum\n$min")
+    bw.flush()
+    bw.close()
+}
