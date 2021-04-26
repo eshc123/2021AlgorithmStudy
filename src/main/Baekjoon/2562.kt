d0ffe69ec225bee0dacf7e5d6d053e45bd1611ad
eshc@naver.com <eshc@naver.com>
2021-04-27 07:08:26 +0900

    2562

--- src/main/Baekjoon/2562.kt
+++ src/main/Baekjoon/2562.kt
@@ -17,3 +17,20 @@ fun main() {
     bw.flush()
     bw.close()
 }
+
+//import java.util.*
+//import java.io.*
+//
+//fun main() {
+//    val br = BufferedReader(InputStreamReader(System.`in`))
+//    val bw = BufferedWriter(OutputStreamWriter(System.out))
+//    val array = Array(100) { 0 }
+//    var max = 0
+//    for(i in 0..8) {
+//        val a = StringTokenizer(br.readLine()).nextToken().toInt()
+//        array[a] = i + 1
+//        if (a > max) max = a
+//    }
+//    bw.write(max.toString()+"\n" + array[max].toString())
+//    bw.flush()
+//}
