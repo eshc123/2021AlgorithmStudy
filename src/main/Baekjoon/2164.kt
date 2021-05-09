b2a118a469408bb4ae5b29428dca7058db09ccec
eshc@naver.com <eshc@naver.com>
2021-05-10 07:08:57 +0900

    2164

---
+++ src/main/Baekjoon/2164.kt
@@ -0,0 +1,17 @@
+import java.util.*
+import java.io.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val n = br.readLine().toInt()
+
+    val queue : Queue<Int> = LinkedList()
+    for(i in 1..n ){
+        queue.offer(i)
+    }
+    while(queue.size > 1 ){
+        queue.poll()
+        queue.offer(queue.poll())
+    }
+    println(queue.poll())
+}
