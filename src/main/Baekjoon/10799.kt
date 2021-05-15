fe2912b31aad45d89f39b2f21ed038ee7750e87e
eshc@naver.com <eshc@naver.com>
2021-05-15 18:27:55 +0900

    10799

---
+++ src/main/Baekjoon/10799.kt
@@ -0,0 +1,39 @@
+import java.io.*
+import java.lang.StringBuilder
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    val s = br.readLine()
+    val stack = Stack<Char>()
+    var result = 0
+    var cur = 0
+    var b =' '
+    for(c in s){
+        when (c) {
+            '(' -> {
+                stack.add(c)
+                cur+=1
+            }
+            ')' -> {
+                val pop = stack.pop()
+                cur -= 1
+                if(b=='(') {
+                    if (stack.isEmpty()) {
+                        continue
+                    } else {
+                        result += cur
+                    }
+                }
+                else {
+                    result +=1
+                }
+            }
+        }
+        b = c
+    }
+    print(result)
+    bw.flush()
+    bw.close()
+}
