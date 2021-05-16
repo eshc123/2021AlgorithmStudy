d7f51cbd27679a81d6bd61b32110842424c44ce3
eshc@naver.com <eshc@naver.com>
2021-05-17 07:38:04 +0900

    rename,refactor

--- src/main/DataStructure/SimpleLinkedList.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    val mx = 1000005
-    val dat[mx], pre[mx], nxt[mx]
-    val unused = 1
-
-
-}
---
+++ src/main/PS/dataStructure/SimpleLinkedList.kt
@@ -0,0 +1,7 @@
+package dataStructure
+
+fun main() {
+    val mx = 1000005
+    //val dat[mx], pre[mx], nxt[mx] = 0,0,0
+    val unused = 1
+}
